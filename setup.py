baad58462864142b3407831e26a01b2e359250d4
Blob2763 <blob2763@gmail.com>
2024-04-08 16:57:36 +0100

    made something slightly more readable

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 
+NAME = "easydebugger"
 VERSION = '1.0.1'
 DESCRIPTION = 'A clean Python debugging tool'
 LONG_DESCRIPTION = 'A clean Python debugging tool. Documentation at https://github.com/Blob2763/easydebugger'
 
 # Setting up
 setup(
-    name="easydebugger",
+    name=NAME,
     version=VERSION,
     author="blob2763",
     description=DESCRIPTION,
